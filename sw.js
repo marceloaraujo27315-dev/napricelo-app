f49ea97858c89c3d96e55f469119d74d319f3896
marceloaraujo27315-dev <marceloaraujo27315@gmail.com>
2026-08-15 15:30:38 -0300

    Atualiza cache para gestão de clientes e unidades

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="napricelo-campo-v11";
+const CACHE="napricelo-campo-v12";
 const ASSETS=["./","./index.html","./style.css","./app.js","./cadastros.js","./photos.js","./cloud-history.js","./manifest.json","./ASSINATURAS TECNICOS.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
